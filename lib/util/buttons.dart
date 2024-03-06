4b9ad1637152f3e9ed3899dfcfc818dcf8ac7957
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-07 00:03:13 +0600

    Updated code errors

--- lib/util/buttons.dart
+++ lib/util/buttons.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class Button extends StatelessWidget {
   final String text;
   VoidCallback onPressed;
--- lib/util/dialogBox.dart
+++ lib/util/dialogBox.dart
@@ -3,7 +3,9 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/util/buttons.dart';
 
+// ignore: must_be_immutable
 class DialogBox extends StatelessWidget {
+  // ignore: prefer_typing_uninitialized_variables
   final controller;
   VoidCallback onAddTask;
   VoidCallback onCancel;
